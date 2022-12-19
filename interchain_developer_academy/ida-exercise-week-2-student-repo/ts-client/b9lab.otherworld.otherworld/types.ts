c5f27cc32b21d44df197237e0446ab724b6bf787
Tobias Leinss <tobias.leinss@mway.io>
2022-12-19 20:27:24 +0100

    Remove week 2 exercise

--- interchain_developer_academy/ida-exercise-week-2-student-repo/ts-client/b9lab.otherworld.otherworld/types.ts
+++
@@ -1,7 +0,0 @@
-import { Params } from "./types/otherworld/params"
-
-
-export {     
-    Params,
-    
- }
--- interchain_developer_academy/ida-exercise-week-2-student-repo/x/otherworld/keeper/grpc_query.go
+++
@@ -1,7 +0,0 @@
-package keeper
-
-import (
-	"github.com/b9lab/other-world/x/otherworld/types"
-)
-
-var _ types.QueryServer = Keeper{}
